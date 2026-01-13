fa6b1927981d8c5c69ea2df998be607fb2e92656
meerim0509 <massakunova@gmail.com>
2026-01-13 20:08:45 +0600

    added new sout hello world and new class VisaPayment

--- src/kg/m_bank/payment/QrPayment.java
+++ src/kg/m_bank/payment/QrPayment.java
@@ -4,5 +4,6 @@ public class QrPayment {
     public static void main(String[] args) {
 
         System.out.println("QR Payment");
+        System.out.println("Hello world");
     }
 }
---
+++ src/kg/m_bank/payment/VisaPayment.java
@@ -0,0 +1,7 @@
+package kg.m_bank.payment;
+
+public class VisaPayment {
+    public static void main(String[] args) {
+
+    }
+}
