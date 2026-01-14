859514616f268a9727631f9fbdb0445da6ac8594
meerim0509 <massakunova@gmail.com>
2026-01-14 20:53:36 +0600

    Resolved conflict

---
+++ src/kg/m_bank/delivery/Delivery.java
@@ -0,0 +1,8 @@
+package kg.m_bank.delivery;
+
+public class Delivery {
+    public static void main(String[] args) {
+        System.out.println("Delivery Yandex");
+        System.out.println("Delivery Glovo");
+    }
+}
