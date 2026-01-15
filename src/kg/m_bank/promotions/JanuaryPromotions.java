fe39d9e268a5b54b0fbbd13199d7651115629e63
meerim0509 <massakunova@gmail.com>
2026-01-15 11:18:50 +0600

    Days of the week
    Day2
    
    Day3
    
    Day4
    
    Day5

--- src/kg/m_bank/promotions/JanuaryPromotions.java
+++ src/kg/m_bank/promotions/JanuaryPromotions.java
@@ -10,5 +10,11 @@ public class JanuaryPromotions {
         System.out.println("January123456 ");
         System.out.println("January1234567 ");
         System.out.println("January12345678 ");
+
+        System.out.println("Day1");
+        System.out.println("Day2");
+        System.out.println("Day3");
+        System.out.println("Day4");
+        System.out.println("Day5");
     }
 }
