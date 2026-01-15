f9c095d28b382348a179acc55584f00d458223e0
meerim0509 <massakunova@gmail.com>
2026-01-15 09:39:29 +0600

    January promotion6

--- src/kg/m_bank/promotions/JanuaryPromotions.java
+++ src/kg/m_bank/promotions/JanuaryPromotions.java
@@ -10,5 +10,6 @@ public class JanuaryPromotions {
         System.out.println("January123456 ");
         System.out.println("January1234567 ");
         System.out.println("January12345678 ");
+        System.out.println("January123456789 ");
     }
 }
